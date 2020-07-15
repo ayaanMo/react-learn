aef9f805278f240eca3d9d679904e76d548cb106
ayaan <464958073@qq.com>
2020-07-15 16:55:19 +0800

    增加重定向路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Link, Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
 import Home from './Home';
 import ComponentInfos from './ComponentInfos';
 
@@ -18,14 +18,18 @@ class App extends Component {
                             </div>
                             <div className='nav navbar-nav'>
                                 <li style={{ cursor: 'pointer' }}><Link to='/'>首页</Link></li>
-                                <li style={{ cursor: 'pointer' }}><Link to='/react'>React</Link></li>
+                                <li style={{ cursor: 'pointer' }}><Link to='/react'>React核心概念</Link></li>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className='container-fluid'>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/react" component={ComponentInfos} />
+                    <switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/react" component={ComponentInfos} />
+                        <Redirect to="/" />
+                    </switch>
+
                 </div>
             </Router>
         );
